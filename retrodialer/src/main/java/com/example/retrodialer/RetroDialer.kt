520de9669f7d3caf18cbe9cad00713b43dade898
Kshitij Kumar <kshitij.kumar@trell.in>
2022-01-30 18:22:37 +0530

    dialer rotation working fine

--- retrodialer/src/main/java/com/example/retrodialer/RetroDialer.kt
+++ retrodialer/src/main/java/com/example/retrodialer/RetroDialer.kt
@@ -1,13 +1,15 @@
 package com.example.retrodialer
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
-import kotlin.math.cos
-import kotlin.math.min
-import kotlin.math.sin
+import kotlin.math.*
 
 class RetroDialer @JvmOverloads constructor(
     context: Context,
@@ -28,6 +30,8 @@ class RetroDialer @JvmOverloads constructor(
     private val xCenterOfParentCircle: Float get() = finalWidth/2f
     private val yCenterOfParentCircle: Float get() = finalHeight/2f
     private val mainCircleRadius: Float get() = min(finalHeight, finalWidth)/2f
+
+    private var dialerRotatedAngle = 0
 
     private val mainCirclePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.WHITE
@@ -72,14 +76,14 @@ class RetroDialer @JvmOverloads constructor(
         super.onDraw(canvas)
         canvas?.let {
 
+            mainCirclePaint.color = Color.WHITE
             canvas.drawCircle(xCenterOfParentCircle, yCenterOfParentCircle, mainCircleRadius, mainCirclePaint)
             printTextOnDialer(it, 180.toDouble(), textPaint, 1)
 
             mainCirclePaint.color = Color.RED
             internalCanvas.drawCircle(xCenterOfParentCircle, yCenterOfParentCircle, mainCircleRadius, mainCirclePaint)
 
-            val angleRotated = 0
-            nextHoleInCircle(internalCanvas, (180 + angleRotated).toDouble(), holePaint, 1)
+            nextHoleInCircle(internalCanvas, (180 + dialerRotatedAngle).toDouble(), holePaint, 1)
 
             canvas.drawBitmap(internalBitmap, 0f, 0f, null)
 
@@ -95,7 +99,7 @@ class RetroDialer @JvmOverloads constructor(
 
         holeCanvas.drawCircle(newX, newY, HOLE_RADIUS, paint)
 
-        nextHoleInCircle(holeCanvas, angle-30, paint, count+1)
+        nextHoleInCircle(holeCanvas, angle+30, paint, count+1)
     }
 
     private fun printTextOnDialer(textCanvas: Canvas, angle: Double, paint: Paint, count: Int) {
@@ -107,7 +111,60 @@ class RetroDialer @JvmOverloads constructor(
 
         textCanvas.drawText((count-1).toString(), newX-10, newY+5, paint)
 
-        printTextOnDialer(textCanvas, angle-30, paint, count+1)
+        printTextOnDialer(textCanvas, angle+30, paint, count+1)
+    }
+
+    private fun angleBetweenTwoLines(x1: Float, y1: Float, x2: Float, y2: Float) {
+        val m1 = (y2 - yCenterOfParentCircle) / (x2 - xCenterOfParentCircle)
+        val m2 = (y1 - yCenterOfParentCircle) / (x1 - xCenterOfParentCircle)
+
+        val tanTheta = (m2 - m1) / (1 + m1*m2)
+
+        val angle = atan(tanTheta.toDouble())
+
+        val pureRotation = Math.toDegrees(angle).toInt()
+        if ((abs(dialerRotatedAngle + pureRotation) >= 340) || dialerRotatedAngle + pureRotation >= 0) {
+            return
+        }
+        dialerRotatedAngle += pureRotation
+        dx = x2
+        dy = y2
+        Log.d("RotatingAngle", "ang: $dialerRotatedAngle")
+        invalidate()
+    }
+
+    private var dx = 0f
+    private var dy = 0f
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                dx = event.x
+                dy = event.y
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                angleBetweenTwoLines(dx, dy, event.x, event.y)
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                reverse()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun reverse() {
+        ValueAnimator.ofInt(dialerRotatedAngle, 0).apply {
+            addUpdateListener {
+                val currentValue = it.animatedValue.toString().toInt()
+                dialerRotatedAngle = currentValue
+                invalidate()
+            }
+            start()
+        }
     }
 
 }
